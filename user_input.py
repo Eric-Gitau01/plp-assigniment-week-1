a20b01d41f5132e1587e5d27a22c7f3ba2aa0aac
Eric Gitau <gitaueric01@gmail.com>
2024-11-06 17:00:43 +0300

    week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+age = input("what is your age?")
+location = input("what is your location?")
+
+print(f"hello {name}, are {age} years old my location {location}")
